633f1e4485606067abe30f95fd9d506197f56b2f
Matias <mmilocik@fi.uba.ar>
2025-12-24 12:59:22 -0300

    CORS para conectar con el frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,15 @@ import restaurantesRoutes from "./routes/restaurantes.js";
 const app = express();
 app.use(express.json());
 
+// CORS 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  next();
+});
+
+// Rutas
 app.use("/restaurantes", restaurantesRoutes);
 
 const PORT = process.env.PORT || 3000;
