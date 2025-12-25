93ba1bd4ecccd4ff5b4089ab0f48aa99cc7c7422
matimilo5 <mmilocik@fi.uba.ar>
2025-12-25 10:43:18 -0300

    importar reservas

--- backend/src/routes/reservas.js
+++ backend/src/routes/reservas.js
@@ -1,1 +1,11 @@
+import express from "express";
+import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
+const { Pool } = pkg;
+const router = express.Router();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
